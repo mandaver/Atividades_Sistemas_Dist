72adc80469dd25005fc5d8983c21a49fab092a38
Victor Roberto <90845665+mandaver@users.noreply.github.com>
2023-05-17 19:57:32 -0400

    cliente.py

--- Atividade_4/cliente.py
+++ Atividade_4/cliente.py
@@ -1,23 +1,18 @@
-import grpc
-import remote_control_pb2
-import remote_control_pb2_grpc
+import click
+import grpc  # Importação do módulo grpc para comunicação gRPC
+import my_service_pb2  # Importação do módulo gerado pelo protocol buffer
+import my_service_pb2_grpc  # Importação do módulo gerado pelo protocol buffer para suporte a gRPC
 
-def run():
-    # Cria um canal gRPC para se comunicar com o servidor
-    with grpc.insecure_channel("localhost:50051") as channel:
-        # Cria um stub do serviço RemoteControl
-        stub = remote_control_pb2_grpc.RemoteControlStub(channel)
-        # Loop principal do cliente
-        while True:
-            # Lê o comando inserido pelo usuário
-            command = input("Insira um comando para executar (ou 'exit' para sair): ")
-            # Verifica se o usuário deseja sair
-            if command == "exit":
-                break
-            # Envia o comando para o servidor e recebe a resposta
-            response = stub.ExecuteCommand(remote_control_pb2.CommandRequest(command=command))
-            # Exibe o resultado da execução do comando
-            print(response.output)
+@click.command()
+@click.option('--server', default='localhost:50051', help='O endereço do servidor gRPC')
+def execute_command(server):
+    channel = grpc.insecure_channel(server)  # Cria um canal de comunicação gRPC não seguro
+    stub = my_service_pb2_grpc.RemoteControlStub(channel)  # Cria um stub para a interface gRPC
+    while True:
+        command = input('Digite o comando que deseja executar: ')  # Solicita ao usuário para digitar o comando
+        response = stub.ExecuteCommand(my_service_pb2.CommandRequest(command=command))  # Chama o método remoto ExecuteCommand e passa o comando digitado
+        print(response.output)  # Exibe a saída recebida do servidor
 
-if __name__ == "__main__":
-    run()
+if __name__ == '__main__':
+    execute_command()  # Executa o comando principal quando o script é executado diretamente
+
