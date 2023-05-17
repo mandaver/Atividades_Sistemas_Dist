2a2e2b5b70f3dde19166290c7b8206ac47554a8a
Victor Roberto <90845665+mandaver@users.noreply.github.com>
2023-05-17 19:27:16 -0400

    consumidor.py

--- Atividade_2/consumidor.py
+++ Atividade_2/consumidor.py
@@ -1,36 +1,27 @@
 import pika
-import subprocess
+from termcolor import colored
 
 # Conecta ao RabbitMQ
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))  # Conecta ao servidor RabbitMQ local
+channel = connection.channel()  # Cria um canal de comunicação
 
-# Declara os tópicos
-channel.exchange_declare(exchange='cpu_temperature', exchange_type='fanout')
-channel.exchange_declare(exchange='fire_detection', exchange_type='fanout')
-channel.exchange_declare(exchange='fire_prevention', exchange_type='fanout')
+# Declara o tópico de temperatura e o tópico de incêndio
+channel.queue_declare(queue='temperature')  # Declara a fila "temperature" para receber mensagens de temperatura
+channel.queue_declare(queue='fire_alert')  # Declara a fila "fire_alert" para receber mensagens de alerta de incêndio
 
-# Define o limite de temperatura
-temperature_limit = 70
-
-# Cria a fila e se inscreve no tópico
-result = channel.queue_declare(queue='', exclusive=True)
-queue_name = result.method.queue
-channel.queue_bind(exchange='cpu_temperature', queue=queue_name)
-
-# Verifica a temperatura a cada mensagem recebida
+# Define a função de callback para lidar com as mensagens recebidas
 def callback(ch, method, properties, body):
     temperature = float(body)
-    if temperature > temperature_limit:
-        # Publica mensagem de detecção de incêndio
-        channel.basic_publish(exchange='fire_detection', routing_key='', body='Fire detected!')
+    if temperature > 50:
+        # Se a temperatura for maior que 50 graus Celsius, publica uma mensagem no tópico de incêndio
+        channel.basic_publish(exchange='', routing_key='fire_alert', body='FIRE DETECTED!')
+        print(colored('Incêndio detectado!', 'red'))  # Exibe uma mensagem de incêndio detectado em vermelho
+    else:
+        print('Temperature:', temperature)  # Exibe a temperatura normalmente
 
-        # Publica mensagem de ativação do sistema de prevenção de incêndio
-        channel.basic_publish(exchange='fire_prevention', routing_key='', body='Activate fire prevention system!')
+# Registra a função de callback no tópico de temperatura
+channel.basic_consume(queue='temperature', on_message_callback=callback, auto_ack=True)
 
-        # Dispara alarme sonoro ou luminoso (não implementado)
-        print('Fire detected! Activate fire prevention system!')
-
-# Se inscreve na fila e começa a consumir as mensagens
-channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-channel.start_consuming()
+# Inicia o loop de espera por mensagens
+print('Aguardando tarefas...')
+channel.start_consuming()  # Inicia o consumo das mensagens do RabbitMQ
